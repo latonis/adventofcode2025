3eee56e9f1016f79553b64e7fd429f39ab41eb4e
Jacob Latonis <jlatonis@me.com>
2025-12-05 01:15:18 -0500

    solve: day 5 part 2

--- 05/solution.exs
+++ 05/solution.exs
@@ -11,14 +11,36 @@ fresh_ranges =
   |> Enum.slice(0..(split_pos - 1))
   |> Enum.map(fn x ->
     [start, stop] = String.split(x, "-")
-    {String.to_integer(start), String.to_integer(stop)}
+    Range.new(String.to_integer(start), String.to_integer(stop))
   end)
+  |> Enum.sort()
 
+# part one
 input_lines
 |> Enum.slice((split_pos + 1)..length(Enum.to_list(input_lines)))
 |> Enum.map(fn x -> String.to_integer(x) end)
 |> Enum.filter(fn x ->
-  Enum.any?(fresh_ranges, fn {start, stop} -> x >= start and x <= stop end)
+  Enum.any?(fresh_ranges, fn range -> x in range end)
 end)
 |> Enum.count()
+|> IO.inspect()
+
+# part two
+fresh_ranges
+|> Enum.reduce([], fn current, acc ->
+  case acc do
+    [] ->
+      [current]
+
+    [prev | tail] ->
+      if current.first <= prev.last + 1 do
+        extended_last = max(prev.last, current.last)
+        [prev.first..extended_last | tail]
+      else
+        [current | acc]
+      end
+  end
+end)
+|> Enum.map(fn r -> Range.size(r) end)
+|> Enum.sum()
 |> IO.inspect()
