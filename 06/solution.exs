e25c240c1dc487c236482f442cfe3e69d233dbc6
Jacob Latonis <jlatonis@me.com>
2025-12-07 16:23:33 -0500

    refactor: day 6 part 2

--- 06/solution.exs
+++ 06/solution.exs
@@ -19,35 +19,13 @@ end)
 |> IO.inspect()
 
 # part 2
-lists =
-  input
-  |> Stream.map(&String.codepoints/1)
-  |> Enum.zip()
-  |> Enum.map(&Tuple.to_list/1)
-  |> Enum.map(fn x -> Enum.filter(x, fn element -> element != " " end) end)
-
-blank_indexes =
-  lists
-  |> Enum.with_index()
-  |> Enum.filter(fn {element, _} -> Enum.all?(element, fn x -> x == " " end) end)
-  |> Enum.map_reduce(0, fn index_t, acc ->
-    end_index = elem(index_t, 1) - 1
-    {{acc, end_index}, end_index + 2}
-  end)
-
-number_array = elem(blank_indexes, 0)
-
-end_acc = elem(blank_indexes, 1)
-
-final_arr = number_array ++ [{end_acc, length(lists)}]
-
-numbers =
-  final_arr
-  |> Enum.map(fn {front, back} ->
-    Enum.slice(lists, front..back)
-  end)
-
-numbers
+input
+|> Stream.map(&String.codepoints/1)
+|> Enum.zip()
+|> Enum.map(&Tuple.to_list/1)
+|> Enum.map(fn x -> Enum.filter(x, fn element -> element != " " end) end)
+|> Enum.chunk_by(fn x -> x == [] end)
+|> Enum.reject(fn x -> x == [[]] end)
 |> Enum.map(fn [first | rest] ->
   {operator, new_int} = List.pop_at(first, -1)
   {operator, rest ++ [new_int]}
