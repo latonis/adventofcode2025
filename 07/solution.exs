d6930ded5da4566f436d049c75687660ce0884a7
Jacob Latonis <jlatonis@me.com>
2025-12-07 18:40:58 -0500

    solve: day 7 part 2

--- 07/solution.exs
+++ 07/solution.exs
@@ -9,11 +9,12 @@ defmodule Grid do
   end
 end
 
-input = File.stream!("input")
+input =
+  File.stream!("input")
+  |> Stream.map(&String.trim/1)
+  |> Stream.map(&String.codepoints/1)
 
 input
-|> Stream.map(&String.trim/1)
-|> Stream.map(&String.codepoints/1)
 |> Enum.reduce({[], nil, 0}, fn current_row, {acc, previous_row, split_count} ->
   current_row_map =
     current_row
@@ -66,3 +67,65 @@ input
 end)
 |> elem(2)
 |> IO.inspect()
+
+grid =
+  input
+  |> Enum.with_index()
+  |> Enum.flat_map(fn {row_list, row_index} ->
+    row_list
+    |> Enum.with_index()
+    |> Enum.map(fn {val, col_index} ->
+      {{row_index, col_index}, val}
+    end)
+  end)
+  |> Map.new()
+
+grid_n = Enum.count(input)
+
+defmodule Pathing do
+  def traverse(starter, grid, n) do
+    memo = %{}
+    {start_x, start_y} = starter
+    walk(start_x, start_y, grid, n, memo) |> IO.inspect()
+  end
+
+  defp walk(row, col, grid, n, memo) do
+    case Map.fetch(memo, {row, col}) do
+      {:ok, result} ->
+        {result, memo}
+
+      :error ->
+        if row == n - 1 do
+          {1, Map.put(memo, {row, col}, 1)}
+        else
+          # must go down first
+          case Map.get(grid, {row + 1, col}) do
+            # if beneath is ., go down, only option (x, y + 1)
+            "." ->
+              {res, memo_d} = walk(row + 1, col, grid, n, memo)
+
+              {res, Map.put(memo_d, {row + 1, col}, res)}
+
+            # if ^ beneath, go down and left (x+1, y+1) + go down  and right (x+1, y+1)
+            "^" ->
+              {left, memo_l} = walk(row + 1, col - 1, grid, n, memo)
+
+              {right, memo_r} =
+                walk(row + 1, col + 1, grid, n, memo_l |> Map.put({row + 1, col - 1}, left))
+
+              memo_2 =
+                memo_r |> Map.put({row + 1, col + 1}, right) |> Map.put({row, col}, left + right)
+
+              {left + right, memo_2}
+
+            # else no path
+            _ ->
+              {0, Map.put(memo, {row, col}, 0)}
+          end
+        end
+    end
+  end
+end
+
+{starting_pos, _} = Enum.find(grid, fn {_key, val} -> val == "S" end)
+Pathing.traverse(starting_pos, grid, grid_n)
